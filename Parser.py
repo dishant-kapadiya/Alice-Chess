d5d5128dc133e6eeae2ff5fca70cb45618236a4f
Dishant Kapadiya <dishantkapadiya@icloud.com>
2016-10-05 22:04:34 -0400

    Added parser which can look up grammer and generate random valid message

---
+++ Parser.py
@@ -0,0 +1,42 @@
+import random
+import sys
+
+lines = [line.rstrip('\n').split("~~~") for line in open("message-grammer.txt")]
+lines[-1][-1] = '\n'
+
+
+CFG = dict()
+
+for line in lines:
+    if not line[0] == "":
+        if line[0] not in CFG.keys():
+            CFG[line[0]] = list()
+        # if line[0] == '<message>':
+        #     abc = [tuple(temp.split("~")) for temp in line[1:]]
+        #     print abc
+        xyz = []
+        for temp in line[1:]:
+            a = tuple(temp.split("~"))
+            CFG[line[0]].append(a)
+
+
+sentence = []
+
+def generate(symbol):
+    if symbol not in CFG.keys():
+        sentence.append(symbol)
+        return
+
+    for transitional_element in CFG[symbol][random.randrange(0,len(CFG[symbol]))]:
+        generate(transitional_element)
+
+    return "".join(sentence)
+
+
+
+CFG['<player>'] = [('black',)]
+for i in range(int(sys.argv[1])):
+    print generate('<playermsg>')
+    sentence = []
+# for temp in CFG:
+#      sys.stdout.write(temp+" : "+str(CFG[temp])+"\n")
