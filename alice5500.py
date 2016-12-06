da850aca3c3057a83855af51dfdde926a07290f9
Dishant Kapadiya <dishantkapadiya@icloud.com>
2016-12-05 20:24:40 -0500

    Added MinMax algorithm

--- alice5500.py
+++ alice5500.py
@@ -6,6 +6,82 @@ from aliceengine import *
 
 """Generating sequence of meanigful messages"""
 
+max_depth = 2
+def does_this_move_creates_check(state, move):
+    partial_move_transition = state.current_player.make_move_without_changing_board(move)
+    if partial_move_transition.move_status == MoveStatus.LEAVES_KING_IN_CHECK:
+        return True
+    return False
+
+
+def analyse_state(state):
+    player = state.current_player
+    player_legal_moves = []
+    for move in player.legal_moves:
+        if not does_this_move_creates_check(state, move):
+            player_legal_moves.append(move)
+    player.legal_moves = player_legal_moves
+
+
+def evaluate_state(my_pieces, other_pieces):
+    return sum(my_pieces) - sum(other_pieces)
+
+
+def get_current_and_opponent_pieces(state):
+    if my_team_color == PlayerColor.White:
+        return [state.white_piece, state.black_piece]
+    else:
+        return [state.black_piece, state.white_piece]
+
+
+def min_max(state):
+    legal_moves = state.current_player.legal_moves
+    best_move = legal_moves[0]
+    best_score = float("-inf")
+    for move in legal_moves:
+        next_state = state.current_player.make_move(move)
+        if next_state.move_status == MoveStatus.DONE:
+            analyse_state(next_state.transition_board)
+            score = minimizer(next_state.transition_board)
+            if score > best_score:
+                best_score = score
+                best_move = move
+    return best_move
+
+
+def minimizer(state, depth=1):
+    my_piece, other_piece = get_current_and_opponent_pieces(state)
+    if depth == max_depth:
+        return (5/depth) * evaluate_state(my_piece, other_piece)
+    legal_moves = state.current_player.legal_moves
+    best_score = float("inf")
+    for move in legal_moves:
+        next_state = state.current_player.make_move(move)
+        if next_state.move_status == MoveStatus.DONE:
+            analyse_state(next_state.transition_board)
+            score = maximizer(next_state.transition_board, depth + 1)
+            if score < best_score:
+                best_score = score
+                best_move = move
+    return best_score
+
+
+def maximizer(state, depth=1):
+    my_piece, other_piece = get_current_and_opponent_pieces(state)
+    if depth == max_depth:
+        return (5/depth) * evaluate_state(my_piece, other_piece)
+    legal_moves = state.current_player.legal_moves
+    best_score = float("-inf")
+    for move in legal_moves:
+        next_state = state.current_player.make_move(move)
+        if next_state.move_status == MoveStatus.DONE:
+            analyse_state(next_state.transition_board)
+            score = minimizer(next_state.transition_board, depth + 1)
+            if score > best_score:
+                best_score = score
+                best_move = move
+    return best_score
+
 
 def generate_move_sentence(move):
     list_of_values = list()
@@ -20,30 +96,14 @@ def generate_move_sentence(move):
     return " ".join(list_of_values) + "\n"
 
 
-def does_this_move_creates_check(move):
-    partial_move_transition = game.current_player.make_move_without_changing_board(move)
-    if partial_move_transition.move_status == MoveStatus.LEAVES_KING_IN_CHECK:
-        return True
-    return False
-
-
-def analyse_state():
-    player = game.current_player
-    player_legal_moves = []
-    for move in player.legal_moves:
-        if not does_this_move_creates_check(move):
-            player_legal_moves.append(move)
-    player.legal_moves = player_legal_moves
-
-
 def choose_move():
     player_legal_moves = game.current_player.legal_moves
     if len(player_legal_moves) == 0:
         sys.stdout.write(my_team_color + " surrenders\n")
         sys.exit(0)
-    # TODO: move choosing strategy; currently random
-    move_index = random.randrange(len(player_legal_moves))
-    return player_legal_moves[move_index]
+    move = min_max(game)
+    # move_index = random.randrange(len(player_legal_moves))
+    return move
 
 
 def make_move(move):
@@ -73,10 +133,12 @@ def create_custom_board():
 
 
 end = False
-# game = Board.create_standard_board()
-game = create_custom_board()
-my_team_color = None
-my_team = None
+game = Board.create_standard_board()
+# game = create_custom_board()
+analyse_state(game)
+my_team_color = PlayerColor.White
+my_team = game.white_player
+# print min_max(game)
 debug_file = open('debug.txt', 'w')
 while not end:
     input_message = raw_input()
@@ -88,7 +150,7 @@ while not end:
         else:
             my_team_color = PlayerColor.White
             my_team = game.white_player
-            analyse_state()
+            analyse_state(game)
             move = choose_move()
             game = make_move(move)
             sys.stdout.write(generate_move_sentence(move))
@@ -100,8 +162,8 @@ while not end:
         choose_move()
         game = make_move(move)
         if game.current_player.get_color() == my_team.get_color():
-            analyse_state()
-            print game.current_player.legal_moves
+            analyse_state(game)
+            # print game.current_player.legal_moves
             move = choose_move()
             game = make_move(move)
             sys.stdout.write(generate_move_sentence(move))
@@ -127,6 +189,6 @@ while not end:
         end = True
         sys.exit(0)
     # print choose_move()
-    print game
+    # print game
     sys.stdin.flush()
     sys.stdout.flush()
